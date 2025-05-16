f3f7fbadbe37858537ad923e77904c0c7c1a68bd
arjun singh <arjun.hascode2020@gmail.com>
2025-05-17 03:39:35 +0530

    add entities in basket api

---
+++ Services/Basket/Basket.Core/Entities/BasketCheckout.cs
@@ -0,0 +1,24 @@
+﻿namespace Basket.Core.Entities
+{
+    public class BasketCheckout
+    {
+        public string UserName { get; set; } = string.Empty; // UserName is the unique identifier for the user
+        public decimal TotalPrice { get; set; } // Total price of the items in the basket
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string EmailAddress { get; set; } = string.Empty;
+        public string AddressLine { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+
+        public string CardName { get; set; } = string.Empty;
+        public string CardNumber { get; set; } = string.Empty;
+        public string Expiration { get; set; } = string.Empty;
+        public string CVV { get; set; } = string.Empty;
+        public int PaymentMethod { get; set; } // 0 - Credit Card, 1 - Paypal
+    }
+}
